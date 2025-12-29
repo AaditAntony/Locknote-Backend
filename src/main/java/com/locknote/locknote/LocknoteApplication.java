889409f357a264f3079662b3a9ac81f391cdee88
Aadit Antony <aaditantony1112@gmail.com>
2025-12-29 20:17:42 +0530

    first commit

---
+++ src/main/java/com/locknote/locknote/LocknoteApplication.java
@@ -0,0 +1,13 @@
+package com.locknote.locknote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocknoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocknoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locknote/locknote/LocknoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locknote.locknote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocknoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
