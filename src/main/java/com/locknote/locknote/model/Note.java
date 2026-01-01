3e4d4da063968f9a2e77cae9d9e4afc3466bf658
Aadit Antony <aaditantony1112@gmail.com>
2026-01-01 13:04:21 +0530

    solved the issue of the password hash

--- src/main/java/com/locknote/locknote/model/Note.java
+++ src/main/java/com/locknote/locknote/model/Note.java
@@ -1,5 +1,6 @@
 package com.locknote.locknote.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 @Entity
@@ -19,6 +20,7 @@ public class Note {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
     private User user;
 
     // ===== Getters =====
